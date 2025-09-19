43b456d02fffb75517218483e59f8a29583de0c0
LucasTrentoBortoluzzi <l.bortoluzzi@escola.pr.gov.br>
2025-09-19 08:20:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,57 @@
-const botaoMostrePalavras = document. querySelector('#botaoPalavraChave');
-botaoMostrePalavras.addEventListener('clik', moatraPalavraChave);
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
-function moatraPalavraChave(){
-    const texto = document.querySelector('#esntrada-de-texto').value;
+function mostraPalavrasChave(){
+    const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavraChave = processaTexto(texto);
-    campoResultado.textContent = palavraChave.join(",");
-
+    const palavrasChave = processaTexto(texto);
+   
+    campoResultado.textContent = palavrasChave.join(",");
 
 }
 
+function processaTexto(texto){
+  let palavras = texto.split(/\P{L}+/u);
+  for (let i in palavras){
+    palavras[i] = palavras[i].toLowerCase();
+  }
 
 
+  palavras = tiraPalavrasRuins(palavras);
 
-function processaTexto(texto) {
-    let palavras = texto.split(/\P(L)+/u);
-    return palavras;
+  const frequencias = contaFrequencia(palavras);
+  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+
+  function ordenaPalavra(p1,p2){
+    return frequencias[p2] - frequencias[p1];
+  }
+
+  console.log(ordenadas);
+  return ordenadas.slice(0,10);
+
+
+}
+function contaFrequencia(palavras); {
+  let frequencias = {};
+  for (let i of palavras){
+    frequencias[i] = 0;
+    for(let j of palavras){
+      if (i==j){
+        frequencias[i]++;
+      }
+    }
+  }
+  return frequencias
+}
+function tiraPalavrasRuins(palavras){
+  const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
+  const palavrasBoas = [];
+  for(let palavras of palavras){
+    if(!PALAVRAS_RUINS.has(palavras)&& palavra.lenght > 2){
+      palavrasBoas.push(palavras);
+    }
+
+  }
+  return palavrasBoas;
 }
